d45265ba773df583ba186b3743b4f58a27e29d25
root <blueoyw@naver.com>
2018-11-27 16:32:36 +0900

    modify script

--- vim/vim_config_py.sh
+++ vim/vim_config_py.sh
@@ -1,14 +1,16 @@
 #!/bin/sh
 
-sudo curl -L https://copr.fedorainfracloud.org/coprs/mcepl/vim8/repo/epel-7/mcepl-vim8-epel-7.repo -o /etc/yum.repos.d/mcepl-vim8-epel-7.repo
+#sudo curl -L https://copr.fedorainfracloud.org/coprs/mcepl/vim8/repo/epel-7/mcepl-vim8-epel-7.repo -o /etc/yum.repos.d/mcepl-vim8-epel-7.repo
 
-sudo yum update vim*
+#sudo yum update vim*
 
-HOME=/home/ywoh/linux_script/vim
+#HOME=/home/ywoh/linux_script/vim
 
-cd 
-git clone https://github.com/blueoyw/linux_script.git
+#cd 
+#git clone https://github.com/blueoyw/linux_script.git
 #sudo yum install git ctags
+cp ../bashrc ~/.bashrc
+. ~/.bashrc
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-cp $HOME/yw.vim ~/.vimrc
+cp ./yw.vim ~/.vimrc
 vim +PluginInstall +qall
